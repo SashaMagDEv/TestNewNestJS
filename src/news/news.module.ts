9263a7b1c28f973ce7deaaf952023cd2c82661d6
SashaMagDEv <magnuh10@gmail.com>
2024-07-17 11:24:28 +0300

    update

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NewsService } from '../services/news.service';
 import { NewsController } from '../controllers/news.controller';
+import {CategoryService} from "../services/category.service";
 
 @Module({
-    providers: [NewsService],
+    providers: [NewsService, CategoryService],
     controllers: [NewsController],
 })
 export class NewsModule {}
